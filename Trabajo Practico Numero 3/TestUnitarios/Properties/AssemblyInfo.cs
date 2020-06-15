63c5cf5da08cf99d9393d3ab3252a7a91d9d9813
Matias Cardullo <supermaty97@gmail.com>
2020-06-14 21:22:53 -0300

    TP3 completo

---
+++ Trabajo Practico Numero 3/TestUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3d1e60e7-1af3-4b51-9282-3d43540165cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Trabajo Practico Numero 3/TestUnitarios/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Excepciones;
+using ClasesAbstractas;
+using ClasesInstanciables;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Testea que se lance la Excepcion DniInvalidoException cuando
+        /// se intenta incorporar un alumno con un dni invalido
+        /// </summary>
+        [TestMethod]
+        public void TestDniInvalido()
+        {
+            try
+            {
+                //Arrange and Act
+                Alumno a = new Alumno(2, "Juan", "Perez", "1abc5678", Persona.ENacionalidad.Extranjero, Universidad.EClases.Laboratorio);
+            }
+            catch (Exception e)
+            {
+                //Assert
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+        }
+
+        /// <summary>
+        /// Testea que se lance la Excepcion AlumnoRepetidoException cuando
+        /// se intenta incorporar a la lista de Alumnos de la Universidad
+        /// dos alumnos con el mismo id y el mismo Dni
+        /// </summary>
+        [TestMethod]
+        public void TestAlumnoRepetido()
+        {
+            //Arrange
+            Universidad uni = new Universidad();
+            Alumno a = new Alumno(2, "Juan", "Cruz", "35486456", Persona.ENacionalidad.Argentino, Universidad.EClases.SPD);
+            Alumno a1 = new Alumno(2, "Juan", "Cruz", "35486456", Persona.ENacionalidad.Argentino, Universidad.EClases.SPD); 
+            try
+            {
+                //Act
+                uni += a;
+                uni += a1;
+            }
+            catch (Exception e)
+            {
+                //Assert
+                Assert.IsInstanceOfType(e, typeof(AlumnoRepetidoException));
+            }
+        }
+
+        /// <summary>
+        /// Testea que se lance la Excepcion SinProfesorException al querer crear un objeto Jornada  
+        /// sin haber profesores que tengan el atributo EClases.SPD
+        /// </summary>
+        [TestMethod]
+        public void TestSinProfesor()
+        {
+            //Arrange
+            Universidad uni = new Universidad();
+            //Act
+            try
+            {
+                uni += Universidad.EClases.SPD;
+            }
+            catch (Exception e)
+            {
+                //Assert
+                Assert.IsInstanceOfType(e, typeof(SinProfesorException));
+            }
+
+        }
+
+    }
+}
